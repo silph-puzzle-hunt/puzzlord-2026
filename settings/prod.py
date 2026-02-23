144ecb141a43d76607e578ff5549a723349fa933
Level 51 <levelfiftyone@gmail.com>
2026-02-23 23:55:56 +0800

    Specifically state allowed hosts I GUESS

--- settings/prod.py
+++ settings/prod.py
@@ -4,11 +4,7 @@ from settings.base import SITE_PASSWORD
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = ['puzzlord-2026.onrender.com']
 
 # security checks
 assert SECRET_KEY != "FIXME_SECRET_KEY_GOES_HERE"
